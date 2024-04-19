cc5284a1d1c2451a67e9468d1006965e30ed48a6
Alex Cho <alex.jhcho@gmail.com>
2024-04-19 16:26:39 -0700

    fix: portfolio class

--- portfolio.py
+++ portfolio.py
@@ -1,36 +1,42 @@
-from stock import Stock
-
-
 class Portfolio:
 
-    def __init__(self, name, initial_balance=100000):
+    def __init__(self, name):
         self.name = name
-        self.initial_balance = float(initial_balance)
         self.holdings = {}
 
-    def display_holdings(self):
-        pass
+    def buy(self, ticker, num_shares):
+        if ticker in self.holdings.keys():
+            self.holdings[ticker] += num_shares
+        else:
+            self.holdings[ticker] = num_shares
 
-    def deposit_cash(self):
-        pass
+        print(f'You bought {num_shares} shares of {ticker}')
 
-    def withdraw_cash(self):
-        pass
+    def sell(self, ticker, num_shares):
+        if ticker in self.holdings.keys():
+            if num_shares > self.holdings[ticker]:
+                print("You cannot sell more than you already have.")
+            else:
+                self.holdings[ticker] -= num_shares
+        else:
+            print("You don't own that stock.")
 
-    def get_share_price(self):
-        pass
+        print(f'You sold {num_shares} shares of {ticker}')
 
-    def create_stock_buy_order(self) -> Stock:
-        pass
-
-    def create_stock_sell_order(self) -> Stock:
-        pass
-
-    def buy_stock(self, buy_order, price):
-        pass
-
-    def sell_stock(self, sell_order, price):
-        pass
+    def display_holdings(self) -> dict:
+        print(self.holdings)
+        return self.holdings
 
     def __str__(self):
         return f'This is the account of {self.name}'
+
+
+if __name__ == '__main__':
+    a = Portfolio('alex')
+    a.buy('SOFI', 100)
+    a.buy('AFRM', 50)
+    a.buy('UPST', 40)
+    a.buy('TSLA', 10)
+    a.buy('HOOD', 75)
+    a.buy('LC', 100)
+    a.display_holdings()
--- security.py
+++
@@ -1,6 +0,0 @@
-class Security:
-    def __init__(self, ticker_symbol, **kwargs):
-        self.ticker_symbol = ticker_symbol
-
-    def __str__(self):
-        return self.ticker_symbol
